a99728f2f731b2f8ddc99c9d35328115c96747e3
ahmed shaikh <ahmedshaikh78699@gmail.com>
2020-12-22 21:47:36 +0530

    added pwa

---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const staticDevCoffee = "Ancient-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/*",
+]
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
